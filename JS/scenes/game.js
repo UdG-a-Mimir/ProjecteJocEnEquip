3401873917718d70ec925414ae7d5c1a67c59e7f
u1973231 <99675672+u1973231@users.noreply.github.com>
2022-05-06 15:03:40 +0200

    Comirt

--- JS/scenes/game.js
+++ JS/scenes/game.js
@@ -2,14 +2,19 @@ class GameScene extends Phaser.Scene {
 
     constructor (){
        super('GameScene');
+
+       //Etiquetas
+       this.puntos = 0
     }
 
     preload (){	
-		
+        //Color del fondo
+        this.cameras.main.setBackgroundColor("#d1d1d1")
 	}
 	
     create (){	
 		
+      
 	}
 	
 	update (){
