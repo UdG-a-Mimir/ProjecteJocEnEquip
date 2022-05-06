41f7e39c35ea12ce0fe161636995c70f4a1dd23f
u1973231 <99675672+u1973231@users.noreply.github.com>
2022-05-06 16:25:05 +0200

    Comit Sistema de guardado
    Metodos para guardar partida establecidos. desde el LocalStorage

--- JS/scenes/game.js
+++ JS/scenes/game.js
@@ -1,24 +1,58 @@
 class GameScene extends Phaser.Scene {
 
     constructor (){
-       super('GameScene');
+        super('GameScene');
 
-       //Etiquetas
-       this.puntos = 0
+        //Etiquetas
+        this.etiPuntos; 
+        this.etiVidas;
+        
+        //Variables globales
+        this.datosPartida = {
+            puntos:0,
+            vida:3
+        }
+       
+     
     }
 
     preload (){	
+
+        //Cargar partida
+        this.carrgarPartida();
+
         //Color del fondo
         this.cameras.main.setBackgroundColor("#d1d1d1")
+
+        //Creacion de Etiquetas
+        this.etiPuntos = this.add.text(16,16, 'Puntos: ' + this.datosPartida.puntos,{fontSize:'32px',fill: '#000'});
+        this.etiVida = this.add.text(600,16, 'Vida: ' + this.datosPartida.vida,{fontSize:'32px',fill: '#000'})
+
 	}
 	
-    create (){	
-		
-      
+    create (){			
+       
 	}
 	
-	update (){
-        
+	update (){    
+        //Creacion de Etiquetas
+        this.etiPuntos.text = "Puntos: " + this.datosPartida.puntos;
+        this.etiVida.text = "Vida: " + this.datosPartida.vida;
+
+    }
+
+    
+    //Carga la partida del localStorage
+    carrgarPartida(){
+         //Accedo a la configuración de las opciones
+         var json = localStorage.getItem("datosPartida") || '{"puntos":100,"vida":3}';
+         console.log(json);
+         this.datosPartida = JSON.parse(json);
+    }
+    //Guarda la partida a localStorage
+    guardarPartida(){
+       localStorage.setItem("datosPartida",JSON.stringify(this.datosPartida));
     }
 }
+
 
